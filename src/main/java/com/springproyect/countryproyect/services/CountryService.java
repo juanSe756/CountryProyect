61d64ed23cac3a1eacb55c7f43352e8425e38dc2
juanSe756 <juansebastiangilborda@gmail.com>
2024-04-11 17:29:30 -0500

    CountryService modified

--- src/main/java/com/springproyect/countryproyect/services/CountryService.java
+++ src/main/java/com/springproyect/countryproyect/services/CountryService.java
@@ -6,30 +6,24 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
+import java.io.InputStream;
 import java.util.Random;
 
 @Service
 public class CountryService {
-    Random random = new Random();
-//    private final RestTemplate restTemplate;
+
+    private final Random random = new Random();
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Value("classpath:data/countries.json")
     private Resource countriesResource;
 
-    private final ObjectMapper objectMapper = new ObjectMapper();
-//    @Autowired
-//    public CountryService(RestTemplate restTemplate) {
-//        this.restTemplate = restTemplate;
-//    }
-
     public Country getRandomCountry() {
-        try {
-            List<Country> countries = Arrays.asList(objectMapper.readValue(countriesResource.getFile(), Country[].class));
-            if (!countries.isEmpty()) {
-                int randomIndex = random.nextInt(countries.size());
-                return countries.get(randomIndex);
+        try (InputStream inputStream = countriesResource.getInputStream()) {
+            Country[] countries = objectMapper.readValue(inputStream, Country[].class);
+            if (countries.length > 0) {
+                int randomIndex = random.nextInt(countries.length);
+                return countries[randomIndex];
             }
         } catch (IOException e) {
             e.printStackTrace();
