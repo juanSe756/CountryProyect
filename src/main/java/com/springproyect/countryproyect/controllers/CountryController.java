a5185f34da8cf5cb1dd84f1c9511c930c42836ad
juanSe756 <juansebastiangilborda@gmail.com>
2024-04-18 16:06:05 -0500

    get host

--- src/main/java/com/springproyect/countryproyect/controllers/CountryController.java
+++ src/main/java/com/springproyect/countryproyect/controllers/CountryController.java
@@ -7,15 +7,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @RestController
 @RequestMapping("/countries")
 public class CountryController {
 
     private final CountryService countryService;
-
+    private String host;
     @Autowired
     public CountryController(CountryService countryService) {
         this.countryService = countryService;
+        try {
+            InetAddress address = InetAddress.getLocalHost();
+            host = "http://" + address.getHostAddress();
+            System.out.println("Running at "+host);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
     }
     @GetMapping("/generate")
     public Country getRandomCountry() {
